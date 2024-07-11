e816e363b9105c8a77aa2439cf0925cb6c1e05ed
LanderSanjuan <90868395+Xan424@users.noreply.github.com>
2024-07-11 12:37:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -22,12 +25,49 @@ const button = document.getElementById("menu-button");
             div.style.display = (div.style.display === "block") ? "none" : "block";
         });
 
-        function on() {
-            document.getElementById("pop-up").style.display = "block";
-          }
+
+        var swiper = new Swiper('.mySwiper', {
+            slidesPerView:  1.4,
+            spaceBetween: 24,
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'fraction',
+            },
+            navigation: {
+              nextEl: '.swiper-buttons-next',
+              prevEl: '.swiper-buttons-prev',
+            },
+          });
+
+          window.onscroll = function() {
+            scrollFunction()
+          };
+
+
           
-          function off() {
-            document.getElementById("pop-up").style.display = "none";
-          }
-
-
+          function scrollFunction() {
+            const section1 = document.getElementById('section1');
+            const sectionTop = section1.offsetTop;
+            const sectionHeight = section1.clientHeight;
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        
+            if (scrollTop >= sectionTop && scrollTop < sectionTop + sectionHeight) {
+                document.getElementById("nav-element").style.color = "#ffffff";
+                document.getElementById("nav-element2").style.color = "#ffffff";
+                document.getElementById("nav-element3").style.color = "#ffffff";
+                document.getElementById("nav-element4").style.color = "#ffffff";
+                document.getElementById("nav-element5").style.color = "#ffffff";
+                document.getElementById("nav-element6").style.backgroundColor = "#ffffff";
+                document.getElementById("nav-element7").style.backgroundColor = "#ffffff";
+            } else {
+                document.getElementById("nav-element").style.color = "#061662";
+                document.getElementById("nav-element2").style.color = "#061662";
+                document.getElementById("nav-element3").style.color = "#061662";
+                document.getElementById("nav-element4").style.color = "#061662";
+                document.getElementById("nav-element5").style.color = "#061662";
+                document.getElementById("nav-element6").style.backgroundColor = "#061662";
+                document.getElementById("nav-element7").style.backgroundColor = "#061662";
+            }
+        }
+        
+        window.onscroll = scrollFunction;
