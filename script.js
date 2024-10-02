b0504e26f9d434b6f2fb7743691d6ce32da65dd8
LanderSanjuan <90868395+Xan424@users.noreply.github.com>
2024-10-02 08:36:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-
-
-
-
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
 });
-
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show2');
+      } else {
+          entry.target.classList.remove('show2');
+      }
+  });
+});
+
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
 
 const button = document.getElementById("menu-button");
-        const div = document.getElementById("overlaymenu");
-        button.addEventListener("click", function() {
-            button.classList.toggle("open");
-            div.style.display = (div.style.display === "block") ? "none" : "block";
-        });
+      const div = document.getElementById("overlaymenu");
+      button.addEventListener("click", function() {
+          button.classList.toggle("open");
+          div.style.display = (div.style.display === "block") ? "none" : "block";
+      });
 
 
         var swiper = new Swiper('.mySwiper', {
