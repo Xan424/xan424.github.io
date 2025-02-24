de35131b79f3e016d3c9ac045edb79e2a2876770
LanderSanjuan <90868395+Xan424@users.noreply.github.com>
2025-02-24 09:03:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show2');
-        } else {
-            entry.target.classList.remove('show2');
-        }
-    });
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show2');
+      } else {
+          entry.target.classList.remove('show2');
+      }
+  });
 });
 
 
@@ -30,8 +29,55 @@ hiddenElements2.forEach((el) => observer2.observe(el));
 
 
 const button = document.getElementById("menu-button");
-        const div = document.getElementById("overlaymenu");
-        button.addEventListener("click", function() {
-            button.classList.toggle("open");
-            div.style.display = (div.style.display === "block") ? "none" : "block";
-        });
+      const div = document.getElementById("overlaymenu");
+      button.addEventListener("click", function() {
+          button.classList.toggle("open");
+          div.style.display = (div.style.display === "block") ? "none" : "block";
+      });
+
+
+        var swiper = new Swiper('.mySwiper', {
+            slidesPerView:  1.4,
+            spaceBetween: 24,
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'fraction',
+            },
+            navigation: {
+              nextEl: '.swiper-buttons-next',
+              prevEl: '.swiper-buttons-prev',
+            },
+          });
+
+          window.onscroll = function() {
+            scrollFunction()
+          };
+
+
+          
+          function scrollFunction() {
+            const section1 = document.getElementById('section1');
+            const sectionTop = section1.offsetTop;
+            const sectionHeight = section1.clientHeight;
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        
+            if (scrollTop >= sectionTop && scrollTop < sectionTop + sectionHeight) {
+                document.getElementById("nav-element").style.color = "#ffffff";
+                document.getElementById("nav-element2").style.color = "#ffffff";
+                document.getElementById("nav-element3").style.color = "#ffffff";
+                document.getElementById("nav-element4").style.color = "#ffffff";
+                document.getElementById("nav-element5").style.color = "#ffffff";
+                document.getElementById("nav-element6").style.backgroundColor = "#ffffff";
+                document.getElementById("nav-element7").style.backgroundColor = "#ffffff";
+            } else {
+                document.getElementById("nav-element").style.color = "#061662";
+                document.getElementById("nav-element2").style.color = "#061662";
+                document.getElementById("nav-element3").style.color = "#061662";
+                document.getElementById("nav-element4").style.color = "#061662";
+                document.getElementById("nav-element5").style.color = "#061662";
+                document.getElementById("nav-element6").style.backgroundColor = "#061662";
+                document.getElementById("nav-element7").style.backgroundColor = "#061662";
+            }
+        }
+        
+        window.onscroll = scrollFunction;
